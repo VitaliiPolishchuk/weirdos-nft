1ced40788d256cb6c985865830238a6ee05dc4fb
Vitalii <vitalii0polishchuk@gmail.com>
2022-04-07 14:07:47 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { ThemeProvider } from "styled-components";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+import Faq from "./components/sections/Faq";
+import Home from "./components/sections/Home";
+import Roadmap from "./components/sections/Roadmap";
+import Showcase from "./components/sections/Showcase";
+import Team from "./components/sections/Team";
+import About from "./components/sections/About";
+import GlobalStyles from "./styles/GlobalStyles";
+import { light } from "./styles/Themes";
+import ScrollToTop from "./components/ScrollToTop";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={light}>
+        <Navigation />
+        <Home />
+        <About />
+        <Roadmap />
+        <Showcase />
+        <Team />
+        <Faq />
+        <Footer />
+        <ScrollToTop />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
